6e2baedd67616efa4d61dfea9ca55207e8272fd0
johnobodai <j.obodai@alustudent.com>
2024-01-14 20:56:20 +0000

    Refactor:Adjust code size

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+
 arr = np.array([9, 8, 2, 3, 9, 4, 1, 0, 3])
 arr1, arr2, arr3 = arr[0:2], arr[-5:], arr[1:6]
 print("The first two numbers of the array are: {}".format(arr1))
