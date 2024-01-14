ee2786d8b75fcf6655826a9d7e3c4af399c2cbe6
johnobodai <j.obodai@alustudent.com>
2024-01-14 20:54:39 +0000

    Refactor:Reduce code size

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
-
 arr = np.array([9, 8, 2, 3, 9, 4, 1, 0, 3])
-arr1 = arr[0:2]
-arr2 = arr[-5:]
-arr3 = arr[1:6]
-
+arr1, arr2, arr3 = arr[0:2], arr[-5:], arr[1:6]
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th number of the array are: {}".format(arr3))
-
