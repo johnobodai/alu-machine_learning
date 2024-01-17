ec211b3ddae72e9229a6976724cfd2307aff5a6b
johnobodai <j.obodai@alustudent.com>
2024-01-17 16:25:16 +0000

    Refactor

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,7 +6,7 @@ def matrix_transpose(matrix):
     Parameters:
     - list: The transpose of the input matrix
     """
-    rows, columns = len(martix), len(martix[0])
+    rows, columns = len(matrix), len(matrix[0])
     transposed_matrix = [[0] * row for x in range(columns)]
 
     for i in range(rows):
