85d5111316cbe6a3b7b4176434c72522b95684bd
johnobodai <j.obodai@alustudent.com>
2024-01-20 13:59:51 +0000

    Refactor

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,7 @@ def add_matrices2D(mat1, mat2):
 
     if isinstance(mat1, list) and isinstance(mat2, list):
         final_result = []
-        for i in range(len (mat1)):
+        for i in range(len(mat1)):
             result = [a + b for a, b in zip(mat1[i], mat2[i])]
             final_result.append(result)
 
