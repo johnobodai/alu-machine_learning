a537fec22555b8c1db97f9b94581ad4face35d64
johnobodai <j.obodai@alustudent.com>
2024-01-20 15:03:06 +0000

    Concatenates two matrices along a specific axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,13 @@
+def cat_matrices2D(mat1, mat2, axis=0):
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
+        return None
+    elif axis == 1 and len(mat1) != len(mat2):
+        return None
+
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        result = [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+        return result
+    else:
+        return None
