af5c90c6e9d23c3ecc2f945b8b8a57c508586004
johnobodai <j.obodai@alustudent.com>
2024-01-20 16:52:33 +0000

    Pycodestyle validation

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,10 +9,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Parameters:
     - mat1 (list): The first input 2D matrix.
     - mat2 (list): The second input 2D matrix.
-    - axis (int): The axis along which to concatenate (0 for rows, 1 for columns).
+    - axis (int): The axis along which to concatenate
+                  (0 for rows, 1 for columns).
 
     Returns:
-    - list or None: The result of concatenating mat1 and mat2 along the specified axis,
+    - list or None: The result of concatenating mat1 and mat2
+                    along the specified axis,
                     or None if the matrices cannot be concatenated.
     """
     if axis == 0 and len(mat1[0]) != len(mat2[0]):
