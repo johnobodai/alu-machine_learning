b4b421d38e00202db4ee419e74aeabd720c007c5
johnobodai <j.obodai@alustudent.com>
2024-01-26 16:17:42 +0000

    Add .plot function

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -7,6 +7,7 @@ r = np.log(0.5)
 t = 5730
 y = np.exp((r / t) * x)
 
+plt.plot(x, y, color='blue')
 plt.xlabel('Time (years)')
 plt.ylabel('Fraction Remaining')
 plt.title('Exponential Decay of C-14')
