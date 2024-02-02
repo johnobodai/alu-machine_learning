73d82233fc2df29a8aa4dc3e9027e5d80f476f12
johnobodai <j.obodai@alustudent.com>
2024-02-02 17:01:36 +0000

    Refactor

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -16,7 +16,7 @@ def summation_i_squared(n):
     '''
     if not isinstance(n, int) or n < 0:
         return None
-    if n == 0:
-        return 0
-    else:
-        return n ** 2 + summation_i_squared(n-1)
+
+    result = n * (n + 1) * (2 * n + 1) // 6
+
+    return result
