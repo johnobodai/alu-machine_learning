debef8e3a960f2351b351aec0be3be9a5244b533
johnobodai <j.obodai@alustudent.com>
2024-02-02 15:57:33 +0000

    Calculates the sigma of a squared

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''A function that calculates as Sigma notation'''
+
+def summation_i_squared(n):
+    '''
+    Calculates the sigma squared of an input with a limit.
+
+    Parameters:
+    - n (int): The stopping condition for the sum.
+
+    Returns:
+    - int or None: The interger value of the sum. Returns None if
+                   n is not a valid number
+    '''
+    if not isinstance(n, int) or n < 0:
+        return None
+    if n == 0:
+        return 0
+    else:
+        return n ** 2 + summation_i_squared(n-1)
+'''
+if __name__ == "__main__":
+    n = 5
+    result = summation_i_squared(n)
+    print(result)
+'''
