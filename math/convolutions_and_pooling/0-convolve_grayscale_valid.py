b07cbc6184cf3d292cbb83a3745063d1fca691ae
johnobodai <j.obodai@alustudent.com>
2024-02-16 14:28:48 +0000

    Performs convolution on gray scale images

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+import numpy as np
+
+"""Performs a valid convulution on grayscale images"""
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    Performs a valid convulution on grayscale images.
+
+    Args:
+        images (array): multiple grayscale images
+        kernel (array): the kernel for the convolution
+
+    Returns:
+        array: the convolved image
+    """
+
+    # Dimension of images and kernel.
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    # Shape of the output image.
+    oh = h - kh + 1  # Outputheight
+    ow = w - kw + 1  # Outputweight
+
+    convolved_images = np.zeros((m, oh, ow))
+
+    # For each image.
+    for i in range(m):
+        # For each pixel in image.
+        for j in range(oh):
+            for k in range(ow):
+                region = images[i, j:j+kh, k:k+kw]
+                convolved_value = np.sum(region * kernel)
+                convolved_images[i, j, k] = convolved_value
+
+    return convolved_images
