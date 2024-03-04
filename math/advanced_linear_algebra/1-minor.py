5edf6b4f8aff50a0526dcbf75f873630eaf415c3
johnobodai <j.obodai@alustudent.com>
2024-03-04 19:27:46 +0000

    Refactor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,61 +1,49 @@
 #!/usr/bin/env python3
-"""Calculates the minor matrix of a matrix"""
+"""Getting the minor matrix of a matrix"""
+
+
+def determinant(matrix):
+    """Function that calculates the determinant of a matrix"""
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        determ = ((matrix[0][0] * matrix[1][1])
+                  - (matrix[0][1] * matrix[1][0]))
+        return determ
+
+    determ = 0
+    for i, j in enumerate(matrix[0]):
+        row = [r for r in matrix[1:]]
+        temp = []
+        for r in row:
+            a = []
+            for c in range(len(matrix)):
+                if c != i:
+                    a.append(r[c])
+            temp.append(a)
+        determ += j * (-1) ** i * determinant(temp)
+    return determ
 
 
 def minor(matrix):
-    """
-    Calculate the minor matrix of a matrix.
+    """Function that calculates the minor matrix of a matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if any(not isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if any(len(row) != len(matrix) for row in matrix):
+        raise ValueError('matrix must be a non-empty square matrix')
 
-    Parameters:
-        matrix (list of lists): The input matrix for which the minor
-                                matrix is to be calculated.
+    if len(matrix) == 1:
+        return [[1]]
 
-    Returns:
-        list of lists: The minor matrix of the input matrix.
-
-    Raises:
-        TypeError: If matrix is not a list of lists.
-        ValueError: If matrix is not a square matrix or is empty.
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list)\
-            for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-
-    if not matrix or not all(len(row) == len(matrix) for row in matrix):
-        raise ValueError("matrix must be a non-empty square matrix")
-
-    def determinant(sub_matrix):
-        """
-        Calculate the determinant of a submatrix.
-
-        Parameters:
-            sub_matrix (list of lists): The submatrix for which the
-                        determinant is to be calculated.
-
-        Returns:
-            float: The determinant of the submatrix.
-        """
-        if len(sub_matrix) == 1:
-            return sub_matrix[0][0]
-        elif len(sub_matrix) == 2:
-            return sub_matrix[0][0] * sub_matrix[1][1] -\
-                sub_matrix[0][1] * sub_matrix[1][0]
-        else:
-            det = 0
-            for i in range(len(sub_matrix)):
-                sign = (-1) ** i
-                cofactor = determinant([row[:i] + row[i+1:]\
-                        for row in sub_matrix[1:]])
-                det += sign * sub_matrix[0][i] * cofactor
-            return det
-
-    minor_mat = []
-    for i in range(len(matrix)):
-        row = []
-        for j in range(len(matrix[0])):
-            sub_matrix = [row[:j] + row[j+1:]\
-    for row in (matrix[:i] + matrix[i+1:])]
-            row.append(determinant(sub_matrix))
-        minor_mat.append(row)
-
-    return minor_mat
+    mino = []
+    for x in range(len(matrix)):
+        t = []
+        for y in range(len(matrix[0])):
+            s = []
+            for row in (matrix[:x] + matrix[x + 1:]):
+                s.append(row[:y] + row[y + 1:])
+            t.append(determinant(s))
+        mino.append(t)
+    return mino
