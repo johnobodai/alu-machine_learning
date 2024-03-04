14404b67dfc65ef3fd89a96bc8539725e72eb8dc
johnobodai <j.obodai@alustudent.com>
2024-03-04 17:06:24 +0000

    Minor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -2,33 +2,54 @@
 """Calculates the minor matrix of a matrix"""
 
 
-def calculate_minor_matrix(matrix):
+def minor(matrix):
     """
     Calculate the minor matrix of a matrix.
 
     Parameters:
-        matrix (list of lists): The input matrix.
+        matrix (list of lists): The input matrix for which the minor matrix is to be calculated.
 
     Returns:
         list of lists: The minor matrix of the input matrix.
+
+    Raises:
+        TypeError: If matrix is not a list of lists.
+        ValueError: If matrix is not a square matrix or is empty.
     """
-    if not isinstance(matrix, list) or matrix == []:
-        raise TypeError('matrix must be a list of lists')
-    if any(not isinstance(row, list) for row in matrix):
-        raise TypeError('matrix must be a list of lists')
-    if any(len(row) != len(matrix) for row in matrix):
-        raise ValueError('matrix must be a non-empty square matrix')
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
 
-    if len(matrix) == 1:
-        return [[1]]
+    if not matrix or not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
 
-    minor_matrix = []
-    for x in range(len(matrix)):
-        temp = []
-        for y in range(len(matrix[0])):
-            sub_matrix = []
-            for row in (matrix[:x] + matrix[x + 1:]):
-                sub_matrix.append(row[:y] + row[y + 1:])
-            temp.append(calculate_determinant(sub_matrix))
-        minor_matrix.append(temp)
-    return minor_matrix
+    def determinant(sub_matrix):
+        """
+        Calculate the determinant of a submatrix.
+
+        Parameters:
+            sub_matrix (list of lists): The submatrix for which the determinant is to be calculated.
+
+        Returns:
+            float: The determinant of the submatrix.
+        """
+        if len(sub_matrix) == 1:
+            return sub_matrix[0][0]
+        elif len(sub_matrix) == 2:
+            return sub_matrix[0][0] * sub_matrix[1][1] - sub_matrix[0][1] * sub_matrix[1][0]
+        else:
+            det = 0
+            for i in range(len(sub_matrix)):
+                sign = (-1) ** i
+                cofactor = determinant([row[:i] + row[i+1:] for row in sub_matrix[1:]])
+                det += sign * sub_matrix[0][i] * cofactor
+            return det
+
+    minor_mat = []
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[0])):
+            sub_matrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+            row.append(determinant(sub_matrix))
+        minor_mat.append(row)
+
+    return minor_mat
