019d18564cb6c2b02577cf38a05a53198f3a353b
johnobodai <j.obodai@alustudent.com>
2024-03-04 15:48:35 +0000

    Refactor

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
+"""A function that calculates the determinant of a square matrix."""
+
 def determinant(matrix):
+    """
+    Calculates the determinant of a square matrix.
+
+    This function takes a square matrix as input and returns its determinant.
+
+    Parameters:
+    - matrix (list): The input square matrix for which the determinant is to be calculated.
+
+    Returns:
+    - float: The determinant of the input matrix.
+
+    Raises:
+    - TypeError: If matrix is not a list of lists.
+    - ValueError: If matrix is not square.
+    """
     # Check if matrix is a list of lists
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -23,5 +40,4 @@ def determinant(matrix):
         submatrix = [row[:j] + row[j+1:] for row in matrix[1:]]  # Exclude jth column
         det += sign * matrix[0][j] * determinant(submatrix)
 
-    return det
-
+        return det
