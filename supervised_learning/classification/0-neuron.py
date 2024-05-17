1a7b829195f8989033e0cc0924e1267eb700e089
johnobodai <johnodaiobodai@gmail.com>
2024-05-17 16:55:21 +0000

    Pycode validation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 class Neuron:
     """
     Class that defines a single neuron performing binary classification.
@@ -21,7 +22,7 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
