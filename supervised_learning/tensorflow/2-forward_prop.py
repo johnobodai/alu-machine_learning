ca33907f4724d9dd25f441f58dd622850bfb8c9c
johnobodai <johnodaiobodai@gmail.com>
2024-06-07 03:43:14 +0000

    Creates forward propagation graph

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-"""A module that creates a layer for a neural network."""
+"""A module that creates the forward propagation graph for a neural network."""
 
 
 import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
 
 
-def create_layer(prev, n, activation):
+def forward_prop(x, layer_sizes=[], activations=[]):
     """
-    Creates a layer for a neural network.
+    Creates the forward propagation graph for the neural network.
 
     Args:
-        prev: tensor output of the previous layer
-        n: number of nodes in the layer to create
-        activation: activation function for the layer
+        x: placeholder for the input data
+        layer_sizes: list containing the number of nodes in each layer
+        activations: list containing the activation functions for each layer
 
     Returns:
-        Tensor output of the layer
+        Prediction of the network in tensor form
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=init, name='layer')
-    return layer(prev)
+    prev = x
+    for i in range(len(layer_sizes)):
+        prev = create_layer(prev, layer_sizes[i], activations[i])
+    return prev
