3efbc6546f524ee2453885becefdb66e491d5641
johnobodai <johnodaiobodai@gmail.com>
2024-06-08 14:51:54 +0000

    Refactor

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -17,23 +17,18 @@ def dropout_forward_prop(X, weights, L, keep_prob):
 
     # Loop through each layer
     for i in range(1, L + 1):
-        # Retrieve previous layer output
-        A_prev = cache['A' + str(i - 1)]
-        # Retrieve weights and biases
         W = weights['W' + str(i)]
         b = weights['b' + str(i)]
-        # Calculate Z value for the current layer
-        Z = np.dot(W, A_prev) + b
-        # Apply activation function (tanh for hidden layers, softmax for output layer)
-        if i == L:
-            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-        else:
+        A = cache['A' + str(i - 1)]
+        Z = np.matmul(W, A) + b
+        if i < L:
             A = np.tanh(Z)
-            # Apply dropout
-            D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
-            A = np.multiply(A, D) / keep_prob
+            D = np.random.rand(A.shape[0], A.shape[1])
+            D = np.where(D < keep_prob, 1, 0)
+            A = np.multiply(A, D)
+            A = A / keep_prob
             cache['D' + str(i)] = D
-        # Store layer output in cache
+        else:
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
         cache['A' + str(i)] = A
-
     return cache
