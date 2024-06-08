4ab849303465890991988e3dc774cb74de15f803
johnobodai <johnodaiobodai@gmail.com>
2024-06-08 13:03:39 +0000

    Resolve Error

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -21,7 +21,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     l2_sum = 0
 
     for i in range(1, L + 1):
-        l2_sum += np.sum(np.square(weights[f'W{i}']))
+        l2_sum += np.sum(np.square(weights['W{}'.format(i)]))
 
     l2_cost += (lambtha / (2 * m)) * l2_sum
     return l2_cost
