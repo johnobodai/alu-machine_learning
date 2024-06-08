3166d8eac559bda361c5ae745aec987f081f5678
johnobodai <johnodaiobodai@gmail.com>
2024-06-08 15:34:27 +0000

    Refactor

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -19,19 +19,14 @@ def evaluate(X, Y, save_path):
         Tuple containing the network’s prediction, accuracy, and loss
     """
     with tf.Session() as sess:
-        # Import the meta graph
         saver = tf.train.import_meta_graph(save_path + '.meta')
-        # Restore the model
         saver.restore(sess, save_path)
-
-        # Get tensors from the graph's collection
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         y_pred = tf.get_collection('y_pred')[0]
-        loss = tf.get_collection('loss')[0]
         accuracy = tf.get_collection('accuracy')[0]
-
-        # Evaluate the model
-        pred, acc, cost = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
-
-    return pred, acc, cost
+        loss = tf.get_collection('loss')[0]
+        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
+        ls = sess.run(loss, feed_dict={x: X, y: Y})
+    return pred, acc, ls
