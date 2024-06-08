25006dd740cc5615c15d502a9d679133176a8813
johnobodai <johnodaiobodai@gmail.com>
2024-06-08 13:06:44 +0000

    Pycodestyle validation

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A module that calculates the cost of a neural network with L2 regularization"""
+"""Calculates the cost of a neural network with L2 regularization"""
 
 import numpy as np
 
@@ -10,7 +10,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Args:
         cost: the cost of the network without L2 regularization
         lambtha: the regularization parameter
-        weights: a dictionary of the weights and biases (numpy.ndarrays) of the neural network
+        weights: a dictionary of the weights and biases (numpy.ndarrays)
+        of the neural network
         L: the number of layers in the neural network
         m: the number of data points used
 
