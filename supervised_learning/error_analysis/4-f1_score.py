87b05d7645c38e8f14eedb18f10baf413810ebb9
johnobodai <johnodaiobodai@gmail.com>
2024-06-21 03:57:40 +0000

    Refactor

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -8,19 +8,9 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
-    """
-    Calculates the F1 score for each class in a confusion matrix.
-
-    Args:
-        confusion (np.ndarray): Confusion matrix of shape (classes, classes)
-                                where row indices represent the correct labels
-                                and column indices represent the predicted labels.
-
-    Returns:
-        np.ndarray: Array of shape (classes,) containing the F1 score of each class.
-    """
-    sens = sensitivity(confusion)
-    prec = precision(confusion)
-    f1 = 2 * (prec * sens) / (prec + sens)
-    return f1
+    """Calculating the F1 score of a confusion matrix"""
+    a = precision(confusion)
+    b = sensitivity(confusion)
+    return 2 * (a * b) / (a + b)
