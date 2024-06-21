5b6ea9b1288715ea9bab50fb32e901ad66172fe8
johnobodai <johnodaiobodai@gmail.com>
2024-06-21 04:00:10 +0000

    Sensitivity

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
-"""
-Creates a confusion matrix.
-"""
-
+"""Sensitivity"""
 
 import numpy as np
 
-def create_confusion_matrix(labels, logits):
-    """
-    Creates a confusion matrix.
 
-    Args:
-        labels (np.ndarray): One-hot numpy array of shape (m, classes)
-                             with the correct labels.
-        logits (np.ndarray): One-hot numpy array of shape (m, classes)
-                             with the predicted labels.
-
-    Returns:
-        np.ndarray: Confusion matrix of shape (classes, classes).
-    """
-    true_labels = np.argmax(labels, axis=1)
-    predicted_labels = np.argmax(logits, axis=1)
-    num_classes = labels.shape[1]
-    confusion_matrix = np.zeros((num_classes, num_classes))
-    for true, pred in zip(true_labels, predicted_labels):
-        confusion_matrix[true, pred] += 1
-    return confusion_matrix
+def sensitivity(confusion):
+    """Calculating the sensitivity for each class in a confusion matrix"""
+    return np.diag(confusion) / np.sum(confusion, axis=1)
