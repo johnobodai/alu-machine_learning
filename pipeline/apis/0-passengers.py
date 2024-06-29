2069d41a07f7a86f07be0293fe2400d99c064433
johnobodai <johnodaiobodai@gmail.com>
2024-06-29 12:47:51 +0000

    Refactor

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -2,22 +2,21 @@
 """Retrieves ships that can hold a given number of passengers."""
 import requests
 
-
 def availableShips(passengerCount):
     """Returns list of ships that can hold given number of passengers."""
     url = "https://swapi.dev/api/starships/"
     ships = []
-
+    
     while url:
         response = requests.get(url)
         data = response.json()
-
+        
         for ship in data['results']:
-            passengers = ship['passengers']
+            passengers = ship['passengers'].replace(',', '')
             if passengers.isdigit() and int(passengers) >= passengerCount:
                 ships.append(ship['name'])
-
+        
         url = data['next']
-
+    
     return ships
 
