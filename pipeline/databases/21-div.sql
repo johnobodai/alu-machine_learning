6069ed75ca7149e6e7251a387c591de147cd0eea
johnobodai <johnodaiobodai@gmail.com>
2024-07-14 20:21:00 +0000

    Update

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,16 +1,22 @@
-
---divides the first by the second
+-- Function: SafeDiv
+-- Description: Safely divides the first number by the second number or returns 0 if the second number is zero.
+-- Arguments: a - INT, b - INT
+-- Returns: DECIMAL(10, 6)
 DELIMITER //
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS DECIMAL(10, 6)
 BEGIN
-  DECLARE result INT;
+  DECLARE result DECIMAL(10, 6);
+
   IF b = 0 THEN
     SET result = 0;
   ELSE
     SET result = a / b;
   END IF;
+
   RETURN result;
 END //
+
 DELIMITER ;
+
