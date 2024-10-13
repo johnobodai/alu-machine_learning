0f4baf361b87d5ac6efb1d727ecf3fde751195e5
johnobodai <johnodaiobodai@gmail.com>
2024-10-13 10:59:12 +0000

    Fix RNNDecoder

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -45,19 +45,23 @@ class RNNDecoder(tf.keras.layers.Layer):
             Tuple of (y, s).
         """
         # Get the embedding for the input word
-        x = self.embedding(x)
+        x = self.embedding(x)  # Shape: (batch, 1, embedding)
 
         # Use self-attention to get the context vector
         attention = SelfAttention(s_prev.shape[1])
-        context, _ = attention(s_prev, hidden_states)
+        context, _ = attention(s_prev, hidden_states)  # context shape: (batch, units)
+
+        # Reshape context to (batch, 1, units) for concatenation
+        context = tf.expand_dims(context, axis=1)  # Shape: (batch, 1, units)
 
         # Concatenate context with x
-        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
+        x = tf.concat([context, x], axis=-1)  # Shape: (batch, 1, units + embedding)
 
         # Pass through GRU
-        output, s = self.gru(x)
+        output, s = self.gru(x)  # output shape: (batch, 1, units)
 
         # Get the final output word
-        y = self.F(output)
+        y = self.F(output)  # y shape: (batch, 1, vocab)
+        y = tf.squeeze(y, axis=1)  # Shape: (batch, vocab)
 
         return y, s
