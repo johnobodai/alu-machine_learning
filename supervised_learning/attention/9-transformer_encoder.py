2f2eed73262e9cb2338d492f5d1969a24624fdf0
johnobodai <johnodaiobodai@gmail.com>
2024-10-13 14:10:08 +0000

    Create encoder for transformer

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -5,51 +5,37 @@ to create the encoder for a transformer
 """
 
 import tensorflow as tf
-import numpy as np
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """
-    Class to create an encoder for a transformer
+    """Class Encoder for a transformer"""
 
-    class constructor:
-        def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1)
-
-    public instance attributes:
-        N: number of blocks in the encoder
-        dm: dimensionality of the model
-        embedding: embedding layer for the inputs
-        positional_encoding: numpy.ndarray of shape (max_seq_len, dm)
-        blocks: list of length N containing EncoderBlock instances
-        dropout: dropout layer for positional encodings
-
-    public instance method:
-        call(self, x, training, mask):
-            calls the encoder and returns the encoder output
-    """
-
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 max_seq_len, drop_rate=0.1):
         """
         Class constructor
 
-        parameters:
+        Parameters:
             N [int]: number of blocks in the encoder
-            dm [int]: represents the dimensionality of the model
-            h [int]: represents the number of heads
-            hidden [int]: number of hidden units in the fully connected layer
+            dm [int]: dimensionality of the model
+            h [int]: number of heads
+            hidden [int]: number of hidden units in fully connected layer
             input_vocab [int]: size of the input vocabulary
             max_seq_len [int]: maximum sequence length possible
-            drop_rate [float]: the dropout rate
+            drop_rate [float]: dropout rate
 
-        sets the public instance attributes:
+        Sets the following public instance attributes:
             N: number of blocks in the encoder
             dm: dimensionality of the model
             embedding: embedding layer for the inputs
             positional_encoding: numpy.ndarray of shape (max_seq_len, dm)
             blocks: list of length N containing EncoderBlock instances
             dropout: dropout layer for positional encodings
+
+        Returns:
+            None.
         """
         super(Encoder, self).__init__()
         self.N = N
@@ -63,22 +49,23 @@ class Encoder(tf.keras.layers.Layer):
         """
         Calls the encoder and returns the encoder output
 
-        parameters:
+        Parameters:
             x [tensor of shape (batch, input_seq_len)]:
                 contains the input to the encoder
             training [boolean]: determines if the model is in training
             mask: mask to be applied for multi-head attention
 
-        returns:
+        Returns:
             [tensor of shape (batch, input_seq_len, dm)]:
                 contains the encoder output
         """
         seq_len = tf.shape(x)[1]
-        x = self.embedding(x)
-        x += self.positional_encoding[:seq_len, :]
-        x = self.dropout(x, training=training)
+        input_enc = self.embedding(x)
+        input_enc += self.positional_encoding[:seq_len, :]
+        output = self.dropout(input_enc, training=training)
 
         for block in self.blocks:
-            x = block(x, training, mask)
+            output = block(output, training, mask)
 
-        return x
+        return output
+
