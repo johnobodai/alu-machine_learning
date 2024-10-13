679134a2b50b22f42f5cdf8a2c4b2b7afe90b8ec
johnobodai <johnodaiobodai@gmail.com>
2024-10-13 11:40:32 +0000

    Pycode Validation

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -48,17 +48,17 @@ class RNNDecoder(tf.keras.layers.Layer):
         attention = SelfAttention(s_prev.shape[1])
         context_vector, attention_weights = attention(s_prev, hidden_states)
         x_embedded = self.embedding(x)
-        
+
         # Expand context vector to match the dimensions for concatenation
         context_vector_expanded = tf.expand_dims(context_vector, axis=1)
         x_combined = tf.concat([context_vector_expanded, x_embedded], axis=-1)
 
         # Process the combined input through the GRU
         output_sequence, new_hidden_state = self.gru(x_combined)
-        
+
         # Reshape and apply the Dense layer to get the output word
-        output_word = tf.reshape(output_sequence, (-1, output_sequence.shape[2]))
+        output_word = tf.reshape(output_sequence, \
+            (-1, output_sequence.shape[2]))
         output_word = self.F(output_word)
 
         return output_word, new_hidden_state
-
